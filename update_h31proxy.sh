b8f52fb141419a834cf33d46d51e22684fbfb16e
Brad S <brad@horizon31.com>
2021-10-05 10:49:36 -0400

    bump h31proxy to 0.1d and fix bug in update_h31proxy

--- update_h31proxy.sh
+++ update_h31proxy.sh
@@ -2,11 +2,14 @@
 # update H31Proxy by itself
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 
-cp -r patrios_app/h31proxy/h31proxy.net /usr/local/src/h31proxy/.
+$SUDO systemctl stop h31proxy
+$SUDO systemctl stop camera-switcher
 
-echo "H31Proxy updated to version 0.1b"
+cp -r patrios_app/h31proxy/h31proxy.net /usr/local/h31/h31proxy/.
+
+echo "H31Proxy updated to version 0.1d"
 echo "Restarting system services now..."
 
-$SUDO systemctl restart h31proxy
-$SUDO systemctl restart camera-switcher
+$SUDO systemctl start h31proxy
+$SUDO systemctl start camera-switcher
 
